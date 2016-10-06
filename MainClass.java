e77a750570c0fa021f51f7cc8d7248905676ad87
Jean Bouvattier <Jean.Bouvattier@ensimag.grenoble-inp.fr>
2016-10-06 12:35:04 +0200

    jsais pas pourquoi mais moi ça marche

--- MainClass.java
+++ MainClass.java
@@ -1,6 +1,7 @@
 import databasePutain.*;
 
 import java.sql.*;
+import java.util.Scanner;
 
 import geoexplorer.gui.*;
 
@@ -12,6 +13,7 @@ public class MainClass {
 	public static void main(String[] args){
 		System.out.println("hello bitches! It works");
 		question10();
+		question11();
 	}
 	public static void question10(){
 		try {
@@ -22,7 +24,6 @@ public class MainClass {
 			res.next();
 			System.out.println("Nombe d'utilisateur :" + res.getInt(1));
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -31,18 +32,24 @@ public class MainClass {
 			Connection connection;
 			PreparedStatement stmt;
 			//TODO: mettre la bonne requete.
-			stmt = Utils.getConnection().prepareStatement("SELECT COUNT(Id) FROM Users;")
+			stmt = Utils.getConnection().prepareStatement("SELECT tags->'name',St_X(geom),St_Y(geom) FROM nodes WHERE tags->'name' LIKE '" + name + "';");
 			ResultSet res = stmt.executeQuery();
+			System.out.println("WHAT");
 			while (res.next()) {
-			    System.out.println("colonne 1 = " + res.getInt(1) + "; colonne 2 = " + ((PGgeometry) res.getObject(2)).getGeometry());
+			    //System.out.println("colonne 1 = " + res.getInt(1) + "; colonne 2 = " + ((PGgeometry) res.getObject(2)).getGeometry());
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
 	public static void question11(){
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		
+		while(!reader.nextLine().equals("exit")){
+			System.out.println("lol");
+		}
+		System.out.println("exiting");
 		
 	}
 }
